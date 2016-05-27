3cf7a03633686186f19b2104b2eb94d532af8357
m42-k <michaelking21@live.com>
2016-05-27 18:57:54 +1000

    Update force-pie-v2.py

--- force-pie-v2.py
+++ force-pie-v2.py
@@ -3,6 +3,8 @@
 # Written by Limor "Ladyada" Fried for Adafruit Industries, (c) 2015
 # This code is released into the public domain
 
+# HEAVILY MODIFIED BY m42-k!
+
 import time
 import os
 import RPi.GPIO as GPIO
@@ -45,24 +47,28 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
         adcout >>= 1       # first bit is 'null' so drop it
         return adcout
         
-# change these as desired - they're the pins connected from the
-# SPI port on the ADC to the Cobbler
+## GPIO pins used when connecting the ADC to the Raspberry Pi
 SPICLK = 18 
 SPIMISO = 23
 SPIMOSI = 24
 SPICS = 25
 
-# LED GPIO NUMBERS(BCM)
-red_led = 21 # GPIO PIN 40
-green_led = 13 # GPIO PIN 33
-yellow_led = 20 # GPIO PIN 38
+## Small FSR's connected to ADC
+fsr_adc_red = 0; ## Red on  Channel 0
+fsr_adc_yellow = 2; ## Yellow on Channel 2
+fsr_adc_green = 4; ## Green on Channel 4
 
-# SETUP LED GPIO PINS
-GPIO.setup(red_led, GPIO.OUT)
-GPIO.setup(green_led, GPIO.OUT)
-GPIO.setup(yellow_led, GPIO.OUT)
+# LED GPIO numbers(BCM)
+red_led = 21 ## Red - GPIO Pin 40
+yellow_led = 20 # Yellow - GPIO Pin 38
+green_led = 13 # Green - GPIO Pin 33
 
-# set up the SPI interface pins
+# Set up LED GPIO pins
+GPIO.setup(red_led, GPIO.OUT) ## Red
+GPIO.setup(yellow_led, GPIO.OUT) ## Yellow
+GPIO.setup(green_led, GPIO.OUT) ## Green
+
+# Set up the SPI interface pins
 GPIO.setup(SPIMOSI, GPIO.OUT)
 GPIO.setup(SPIMISO, GPIO.IN)
 GPIO.setup(SPICLK, GPIO.OUT)
@@ -77,96 +83,77 @@ def green_block_in():
 
 def yellow_block_in():
         GPIO.output(yellow_led, trim_pot_changed_yellow)
-  
-# 10k trim pot connected to adc #0(RED), #1(GREEN) & #2(YELLOW)
-fsr_adc_red = 0;
-fsr_adc_yellow = 2;
-fsr_adc_green = 4;
 
-## RED
-last_read = 0       # this keeps track of the last potentiometer value
-## YELLOW
-last_read_yellow = 0
-## GREEN
-last_read_green = 0
-# RED & GREEN
-tolerance = 5       # to keep from being jittery we'll only change
-                    # volume when the pot has moved more than 5 'counts'
-# YELLOW
-tolerance_yellow = 5
+## Keep track of the last FSR value
+last_fsr_read_red = 0 ## Red
+last_fsr_read_yellow = 0 ## Yellow
+last_fsr_read_green = 0 ## Green
+
+## Add a tolerance for sensitivity allowances
+tolerance = 5       # to keep from being jittery we'll only do an action
+                    # when the force detected is over this amount
                     
 while True:
-        # we'll assume that the pot didn't move
-        ## RED
-        trim_pot_changed = False
-        ## YELLOW
-        trim_pot_changed_yellow = False
-        ## GREEN
-        trim_pot_changed_green = False
+        ## we'll assume that the blocks haven't been inserted yet
+        block_inserted_red = False ## Red
+        block_inserted_yellow = False ## Yello
+        block_inserted_green = False ## Green
 
-        # read the analog pin
-        ## RED
-        trim_pot = readadc(fsr_adc_red, SPICLK, SPIMOSI, SPIMISO, SPICS)
-        ## YELLOW
-        trim_pot_yellow = readadc(fsr_adc_yellow, SPICLK, SPIMOSI, SPIMISO, SPICS)
-        ## GREEN
-        trim_pot_green = readadc(fsr_adc_green, SPICLK, SPIMOSI, SPIMISO, SPICS)
-        # how much has it changed since the last read?
-        ## RED
-        pot_adjust = abs(trim_pot - last_read)
-        ## YELLOW
-        pot_adjust_yellow = abs(trim_pot_yellow - last_read_yellow)
-        ## GREEN
-        pot_adjust_green = abs(trim_pot_green - last_read_green)
+        ## Read the signals from the FSR's
+        fsr_signal_red = readadc(fsr_adc_red, SPICLK, SPIMOSI, SPIMISO, SPICS) ## Red
+        fsr_signal_yellow = readadc(fsr_adc_yellow, SPICLK, SPIMOSI, SPIMISO, SPICS) ## Yellow
+        fsr_signal_green = readadc(fsr_adc_green, SPICLK, SPIMOSI, SPIMISO, SPICS) ## Green
+        
+        ## How much has it changed since the last read?
+        fsr_adjust_red = abs(fsr_signal_red - last_fsr_read_red) ## Red
+        fsr_adjust_yellow = abs(fsr_signal_yellow - last_fsr_read_yellow) ## Yellow
+        fsr_adjust_green = abs(fsr_signal_green - last_fsr_read_green) ## Green
         
         if DEBUG:
-                ## RED
-                print "trim_pot:", trim_pot
-                print "pot_adjust:", pot_adjust
-                print "last_read", last_read
-                ## YELLOW
-                print "trim_pot_yellow:", trim_pot_yellow
-                print "pot_adjust_yellow:", pot_adjust_yellow
-                print "last_read_yellow", last_read_yellow
-                ## GREEN
-                print "trim_pot_green:", trim_pot_green
-                print "pot_adjust_green:", pot_adjust_green
-                print "last_read_green", last_read_green
-        ## RED
-        if ( pot_adjust > tolerance ):
-               trim_pot_changed = True
-        ## YELLOW
-        if ( pot_adjust_yellow > tolerance_yellow ):
-               trim_pot_changed_yellow = True
-        ## GREEN
-        if ( pot_adjust_green > tolerance ): 
-               trim_pot_changed_green = True
+                ## Red
+                print "fsr_signal_red:", fsr_signal_red
+                print "fsr_adjust_yellow:", fsr_adjust_yellow
+                print "last_fsr_read_red", last_fsr_read_red
+                ## Yellow
+                print "fsr_signal_yellow:", fsr_signal_yellow
+                print "fsr_adjust_yellow:", fsr_adjust_yellow
+                print "last_fsr_read_yellow", last_fsr_read_yellow
+                ## Green
+                print "fsr_signal_green:", fsr_signal_green
+                print "fsr_adjust_green:", fsr_adjust_green
+                print "last_fsr_read_green", last_fsr_read_green
+        ## Red
+        if ( fsr_adjust_red > tolerance ):
+               block_inserted_red = True
+        ## Yellow
+        if ( fsr_adjust_yellow > tolerance ):
+               block_inserted_yellow = True
+        ## Green
+        if ( fsr_adjust_green > tolerance ): 
+               block_inserted_green = True
 
         if DEBUG:
-                ## RED
-                print "trim_pot_changed", trim_pot_changed
-                ## YELLOW
-                print "trim_pot_changed_yellow", trim_pot_changed_yellow
-                ## GREEN
-                print "trim_pot_changed_green", trim_pot_changed_green
+                print "block_inserted_red", block_inserted_red ## Red
+                print "block_inserted_yellow", block_inserted_yellow ## Yellow
+                print "block_inserted_green", block_inserted_green ## Green
         
         ## RED
-        if ( trim_pot_changed ):
+        if ( block_inserted_red ):
                 red_block_in()
-                set_volume = trim_pot / 10.24           # convert 10bit adc0 (0$
-                set_volume = round(set_volume)          # round out decimal val$
-                set_volume = int(set_volume)            # cast volume as integer
+                #set_volume = trim_pot / 10.24           # convert 10bit adc0 (0-1024) trim pot read into 0-100 volume level
+                #set_volume = round(set_volume)          # round out decimal value
+                #set_volume = int(set_volume)            # cast volume as integer
 
-                print 'Volume = {volume}%' .format(volume = set_volume)
-                set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null' .format(volume = set_volume)
-                os.system(set_vol_cmd)  # set volume
+                #print 'Volume = {volume}%' .format(volume = set_volume)
+                #set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null' .format(volume = set_volume)
+                #os.system(set_vol_cmd)  # set volume
                 
-                if DEBUG:
-                        print "set_volume", set_volume
-                        print "trim_pot_changed", set_volume
+                #if DEBUG:
+                        #print "set_volume", set_volume
+                        #print "trim_pot_changed", set_volume
 
                 # save the potentiometer reading for the next loop
-                last_read = trim_pot
+                last_fsr_read_red = fsr_adjust_red
                 
         ## YELLOW
         if ( trim_pot_changed_yellow ):
