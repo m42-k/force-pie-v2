cb132cdf443d790ee0e739a8c1636ea02cc7bbe0
m42-k <michaelking21@live.com>
2016-05-29 15:26:16 +1000

    Update force-pie-v2.py

--- force-pie-v2.py
+++ force-pie-v2.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 
-# Written by Limor "Ladyada" Fried for Adafruit Industries, (c) 2015
-# This code is released into the public domain 
-
-# HEAVILY MODIFIED BY m42-k! 
-# Raspberry Pi Model B+ Used
+## Base code written by Limor "Ladyada" Fried for Adafruit Industries, (c) 2015
+## This code is released into the public domain 
+## HEAVILY MODIFIED BY m42-k!
+## Raspberry Pi Model B/B+
 
 import time
 import os
 import RPi.GPIO as GPIO
 
-#GPIO.setmode(GPIO.BCM)
-#DEBUG = 1
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+global GPIO
 
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+DEBUG = 0
+
+## read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
 def readadc(adcnum, clockpin, mosipin, misopin, cspin): 
         if ((adcnum > 7) or (adcnum < 0)):
                 return -1
@@ -59,16 +61,12 @@ fsr_adc_red = 1; ## Red on Channel 1
 fsr_adc_yellow = 2; ## Yellow on Channel 2
 fsr_adc_green = 0; ## Green on Channel 0
 
-# LED GPIO numbers(BCM)
+## LED GPIO numbers(BCM)
 red_led = 21; ## Red - GPIO Pin 40
 yellow_led = 20; # Yellow - GPIO Pin 38
 green_led = 13; # Green - GPIO Pin 33
 
-global GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-# Set up LED GPIO pins
+## Set up LED GPIO pins
 GPIO.setup(red_led, GPIO.OUT) ## Red
 GPIO.setup(yellow_led, GPIO.OUT) ## Yellow
 GPIO.setup(green_led, GPIO.OUT) ## Green
@@ -88,25 +86,25 @@ block_inserted_green = False ## Green
 def red_block_in(): ## Red
         global block_inserted_red
         GPIO.output(red_led, True)
-        time.sleep(1)
-        GPIO.output(red_led, False)
-        block_inserted_red = False
+        #time.sleep(1)
+        #GPIO.output(red_led, False)
+        #block_inserted_red = False
         return block_inserted_red
         
 def yellow_block_in(): ## Yellow
         global block_inserted_yellow
         GPIO.output(yellow_led, True)
-        time.sleep(1)
-        GPIO.output(yellow_led, False)
-        block_inserted_yellow = False
+        #time.sleep(1)
+        #GPIO.output(yellow_led, False)
+        #block_inserted_yellow = False
         return block_inserted_yellow
         
 def green_block_in(): ## Green
         global block_inserted_green
         GPIO.output(green_led, True)
-        time.sleep(1)
-        GPIO.output(green_led, False)
-        block_inserted_green = False
+        #time.sleep(1)
+        #GPIO.output(green_led, False)
+        #block_inserted_green = False
         return block_inserted_green
 
 ## Keep track of the last FSR value
@@ -119,7 +117,7 @@ tolerance = 5       # to keep from being jittery we'll only do an action
                     # when the force detected is over this amount
                     
 while True:
-        ## we'll assume that the blocks haven't been inserted yet
+        ## We'll assume that the blocks haven't been inserted yet
         global block_inserted_red
         global block_inserted_yellow
         global block_inserted_green
@@ -132,24 +130,24 @@ while True:
         fsr_signal_yellow = readadc(fsr_adc_yellow, SPICLK, SPIMOSI, SPIMISO, SPICS) ## Yellow
         fsr_signal_green = readadc(fsr_adc_green, SPICLK, SPIMOSI, SPIMISO, SPICS) ## Green
         
-        ## How much has it changed since the last read?
+        ## How much has the FSR reading changed since the last read?
         fsr_adjust_red = abs(fsr_signal_red - last_fsr_read_red) ## Red
         fsr_adjust_yellow = abs(fsr_signal_yellow - last_fsr_read_yellow) ## Yellow
         fsr_adjust_green = abs(fsr_signal_green - last_fsr_read_green) ## Green
         
-        #if DEBUG:
+        if DEBUG:
                 ## Red
-                #print "fsr_signal_red:", fsr_signal_red
-                #print "fsr_adjust_yellow:", fsr_adjust_yellow
-                #print "last_fsr_read_red", last_fsr_read_red
+                print "fsr_signal_red:", fsr_signal_red
+                print "fsr_adjust_yellow:", fsr_adjust_yellow
+                print "last_fsr_read_red", last_fsr_read_red
                 ## Yellow
-                #print "fsr_signal_yellow:", fsr_signal_yellow
-                #print "fsr_adjust_yellow:", fsr_adjust_yellow
-                #print "last_fsr_read_yellow", last_fsr_read_yellow
+                print "fsr_signal_yellow:", fsr_signal_yellow
+                print "fsr_adjust_yellow:", fsr_adjust_yellow
+                print "last_fsr_read_yellow", last_fsr_read_yellow
                 ## Green
-                #print "fsr_signal_green:", fsr_signal_green
-                #print "fsr_adjust_green:", fsr_adjust_green
-                #print "last_fsr_read_green", last_fsr_read_green
+                print "fsr_signal_green:", fsr_signal_green
+                print "fsr_adjust_green:", fsr_adjust_green
+                print "last_fsr_read_green", last_fsr_read_green
         ## Red
         if fsr_adjust_red > tolerance:
                block_inserted_red = True
@@ -160,16 +158,10 @@ while True:
         if fsr_adjust_green > tolerance: 
                block_inserted_green = True
                
-        #if DEBUG:
-                #print "block_inserted_red", block_inserted_red ## Red
-                #print "block_inserted_yellow", block_inserted_yellow ## Yellow
-                #print "block_inserted_green", block_inserted_green ## Green
-        print "Green Sensor Reading:", fsr_signal_green ## Green
-        print "block_inserted_green", block_inserted_green ## Green
-        print "Yellow Sensor Reading:", fsr_signal_yellow ## Yellow
-        print "block_inserted_yellow", block_inserted_yellow ## Yellow
-        print "Red Sensor Reading:", fsr_signal_red ## Red
-        print "block_inserted_red", block_inserted_red ## Red
+        if DEBUG:
+                print "block_inserted_red", block_inserted_red ## Red
+                print "block_inserted_yellow", block_inserted_yellow ## Yellow
+                print "block_inserted_green", block_inserted_green ## Green
         
         ## RED
         if block_inserted_red is True:
@@ -182,9 +174,8 @@ while True:
                 #set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null' .format(volume = set_volume)
                 #os.system(set_vol_cmd)  # set volume
                 
-                #if DEBUG:
-                        #print "set_volume", set_volume
-                        #print "trim_pot_changed", set_volume
+                if DEBUG:
+                        print "fsr_adjust_red", fsr_adjust_red
                         
                 ## Save the FSR reading for the next loop
                 last_fsr_read_red = fsr_signal_red
@@ -192,35 +183,19 @@ while True:
         ## YELLOW
         if block_inserted_yellow is True:
                 yellow_block_in() 
-                #range_100_yellow = fsr_signal_yellow / 10.24           # convert 10bit adc0 (0$
-                #range_100_yellow = round(range_100_yellow)          # round out decimal val$
-                #range_100_yellow = int(range_100_yellow)            # cast volume as integer
                 
-                #print 'Volume = {volume_yellow}%' .format(volume_yellow = set_volume_yellow)
-                #set_vol_cmd_yellow = 'sudo amixer cset numid=1 -- {volume_yellow}% > /dev/null' .format(volume_yellow = set_volume_yellow)
-                #os.system(set_vol_cmd_yellow)  # set volume
-                
-                #if DEBUG:
-                        #print "set_volume_yellow", set_volume_yellow
-                        #print "trim_pot_changed_yellow", set_volume_yellow
+                if DEBUG:
+                        print "fsr_adjust_yellow", fsr_adjust_yellow
                         
                 ## Save the FSR reading for the next loop
                 last_fsr_read_yellow = fsr_signal_yellow
                 
         ## GREEN
         if block_inserted_green is True:
-                green_block_in() 
-                #range_100_green = fsr_signal_green / 10.24           # convert 10bit adc0 (0$
-                #range_100_green = round(range_100_green)          # round out decimal val$
-                #range_100_green = int(range_100_green)            # cast volume as integer
+                green_block_in()
                 
-                #print 'Volume = {volume_green}%' .format(volume_green = set_volume_green)
-                #set_vol_cmd_green = 'sudo amixer cset numid=1 -- {volume_green}% > /dev/null' .format(volume_green = set_volume_green)
-                #os.system(set_vol_cmd_green)  # set volume
-                
-                #if DEBUG:
-                        #print "set_volume_green", set_volume_green
-                        #print "trim_pot_changed_green", set_volume_green
+                if DEBUG:
+                        print "fsr_adjust_green", fsr_adjust_green
                         
                 ## Save the FSR reading for the next loop
                 last_fsr_read_green = fsr_signal_green
@@ -229,6 +204,6 @@ while True:
         time.sleep(0.3)
         
         ## Turn LED's Off
-        #GPIO.output(red_led, False)
-        #GPIO.output(yellow_led, False)
-        #GPIO.output(green_led, False)
+        GPIO.output(red_led, False)
+        GPIO.output(yellow_led, False)
+        GPIO.output(green_led, False)
