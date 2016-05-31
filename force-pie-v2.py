4c21605c7ddd71c8fc03297fa8eae5593aa17602
m42-k <michaelking21@live.com>
2016-05-31 16:05:44 +1000

    Update force-pie-v2.py

--- force-pie-v2.py
+++ force-pie-v2.py
@@ -15,7 +15,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False) 
 
 ## Change to 1 to enable debug prints
-DEBUG = 1
+DEBUG = 0
 
 ## read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
 def readadc(adcnum, clockpin, mosipin, misopin, cspin): 
